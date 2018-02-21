e3be035d5dd78ec2dcf1148df78dc243e355e0d8
Viktor <viktor@ogeman.se>
2018-02-21 11:46:02 +0100

    Test logging output

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    console.log("server log")
+    res.send('Hello World 2!')
+})
 
 app.listen(80, () => console.log('Example app listening on port 80!'))
--- restart.sh
+++ restart.sh
@@ -2,6 +2,6 @@
 set -e
 cd /hackroot/code
 git pull origin master
-killall node nodejs | true
+sudo killall node nodejs nohup | true
 npm install
 sudo nohup node ./index.js &>> /hackroot/server.log&

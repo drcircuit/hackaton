a1d48b41e3524468818e4514a101fbb75265bd8f
DonkeyKong <killcam@DonkeyKong.localdomain>
2018-02-21 17:57:35 +0100

    merge

--- repositories/initialdata.js
+++ repositories/initialdata.js
@@ -1,84 +1,119 @@
 module.exports = {
-    projects: [{
-        "_id": "projid2",
-        "created": "12341235124334",
-        "title": "REd cross project 1",
-        "short_description": "This is a short intro text that can be seen on the landing page",
-        "logo": "http://localhost:8080/img/docsnoborderslogo.png",
-        "img": "http://localhost:8080/img/docsnoborders.jpg",
-        "bigimg": "http://localhost:8080/img/docsnobordersbig.jpg",
-        "country": "Denmark",
-        "location": "Middlefart, Denmark",
-        "requested": "132.23",
-        "funded": "100.1",
-        "due_date": "2134123412342",
-        "status": "planned",
-        "description": "This is the long text to show, formated as markdown?",
-        "contract": "emptycontract",
-        "interested": ["d1", "d2", "d3"]
-    }, {
-        "_id": "projid1",
-        "created": "12d341235124334",
-        "title": "African Outbreak",
-        "short_description": "This is a short intro text that can be seen on the landing page",
-        "logo": "http://localhost:8080/img/docsnoborderslogo.png",
-        "img": "http://localhost:8080/img/docsnoborders.jpg",
-        "bigimg": "http://localhost:8080/img/docsnobordersbig.jpg",
-        "country": "Norway",
-        "location": "Middlefart, Denmark",
-        "requested": "132.23",
-        "funded": "100.1",
-        "due_date": "2134123412342",
-        "description": "This is the long text to show, formated as markdown?",
-        "status": "planned",
-        "contract": "emptycontract2",
-        "interested": ["d1", "d2", "d3"]
+  projects: [
+    {
+      _id: 'projid2',
+      created: '12341235124334',
+      title: 'Sexual violence',
+      short_description:
+        'This is a short intro text that can be seen on the landing page',
+      logo: 'http://localhost:8080/img/docsnoborderslogo.png',
+      img: 'http://localhost:8080/img/docsnoborders.jpg',
+      country: 'Denmark',
+      location: 'Middlefart, Denmark',
+      requested: '132.23',
+      funded: '100.1',
+      due_date: '2134123412342',
+      status: 'planned',
+      description: 'This is the long text to show, formated as markdown?',
+      contract: 'emptycontract',
+      interested: ['d1', 'd2', 'd3']
+    },
+    {
+      _id: 'projid1',
+      created: '12d341235124334',
+      title: 'Acute malnutrition',
+      short_description:
+        'This is a short intro text that can be seen on the landing page',
+      logo: 'http://localhost:8080/img/docsnoborderslogo.png',
+      img: 'http://localhost:8080/img/docsnoborders.jpg',
+      country: 'Norway',
+      location: 'Middlefart, Denmark',
+      requested: '132.23',
+      funded: '100.1',
+      due_date: '2134123412342',
+      description: 'This is the long text to show, formated as markdown?',
+      status: 'planned',
+      contract: 'emptycontract2',
+      interested: ['d1', 'd2', 'd3']
+    },
+    {
+      _id: 'projid3',
+      created: '12341235124334',
+      title: 'REd Einar vaccinations',
+      short_description:
+        'This is a short intro text that can be seen on the landing page',
+      logo: 'http://localhost:8080/img/docsnoborderslogo.png',
+      img: 'http://localhost:8080/img/docsnoborders.jpg',
+      country: 'Denmark',
+      location: 'Middlefart, Denmark',
+      requested: '132.23',
+      funded: '100.1',
+      due_date: '2134123412342',
+      status: 'planned',
+      description: 'This is the long text to show, formated as markdown?',
+      contract: 'emptycontract',
+      interested: ['d1', 'd2', 'd3']
+    },
+    {
+      _id: 'projid4',
+      created: '12341235124334',
+      title: 'Epidemics in NewTechLab',
+      short_description:
+        'This is a short intro text that can be seen on the landing page',
+      logo: 'http://localhost:8080/img/docsnoborderslogo.png',
+      img: 'http://localhost:8080/img/docsnoborders.jpg',
+      country: 'Denmark',
+      location: 'Middlefart, Denmark',
+      requested: '132.23',
+      funded: '100.1',
+      due_date: '2134123412342',
+      status: 'planned',
+      description: 'This is the long text to show, formated as markdown?',
+      contract: 'emptycontract',
+      interested: ['d1', 'd2', 'd3']
     }
-    ],
-    doctors: [
-        {
-            "_id": "d1",
-            "name": "Doctor 1",
-            "img": "http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png",
-            "specialities": [
-                "children",
-                "noses"
-            ]
-        },
-        {
-            "_id": "d2",
-            "name": "Doctor 2",
-            "img": "http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png",
-            "specialities": [
-                "ears",
-                "eyes"
-            ]
-        },
-        {
-            "_id": "d3",
-            "name": "Doctor 3",
-            "img": "http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png",
-            "specialities": [
-                "feet",
-                "eyes"
-            ]
-        }
-    ],
-    funders: [
-        {
-            "_id": "sdfe2",
-            "name": "Espen",
-            "img": "http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png"
-        },
-        {
-            "_id": "sdfe3",
-            "name": "Camilla",
-            "img": "http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png"
-        },
-        {
-            "_id": "sdfe3",
-            "name": "Einar",
-            "img": "http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png"
-        }
-    ],
-}
+  ],
+  doctors: [
+    {
+      _id: 'd1',
+      name: 'Doctor 1',
+      img:
+        'http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png',
+      specialities: ['children', 'noses']
+    },
+    {
+      _id: 'd2',
+      name: 'Doctor 2',
+      img:
+        'http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png',
+      specialities: ['ears', 'eyes']
+    },
+    {
+      _id: 'd3',
+      name: 'Doctor 3',
+      img:
+        'http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png',
+      specialities: ['feet', 'eyes']
+    }
+  ],
+  funders: [
+    {
+      _id: 'sdfe2',
+      name: 'Espen',
+      img:
+        'http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png'
+    },
+    {
+      _id: 'sdfe3',
+      name: 'Camilla',
+      img:
+        'http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png'
+    },
+    {
+      _id: 'sdfe3',
+      name: 'Einar',
+      img:
+        'http://www.newsshare.in/wp-content/uploads/2017/04/Miniclip-8-Ball-Pool-Avatar-11.png'
+    }
+  ]
+};

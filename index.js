ad4574a6ff979af37957546799f4bba451b6aa49
Viktor <viktor@ogeman.se>
2018-02-21 11:38:01 +0100

    Dummy server to reply hellow world

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Server is running")
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(80, () => console.log('Example app listening on port 80!'))

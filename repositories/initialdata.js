74aa93b39d283d7f861f59a34c7c1a1f1e24987b
Viktor <viktor@ogeman.se>
2018-02-21 23:08:12 +0100

    Fix get events

--- repositories/initialdata.js
+++ repositories/initialdata.js
@@ -10,7 +10,7 @@ module.exports = {
         "location": "Middlefart, Denmark",
         "status": "planned",
         "description": "#big project \nThis is the long text to show, formated as markdown?",
-        "contract": "0xbd37A601265d90EC90b9b233991488B90A39a2eb",
+        "contract": "0xd6E816b4EEe55e8C2eB7E3af5B60AaEf53AD256F",
         "interested": ["d1", "d2", "d3"],
         "funders": ["arne","linda","rigmor","wayne","garreth","carlos","joann","nieke"]
     }, {
@@ -25,7 +25,7 @@ module.exports = {
         "doctorsNeeded": 3,
         "description": "#big project \nThis is the long text to show, formated as markdown?",
         "status": "planned",
-        "contract": "0xbd37A601265d90EC90b9b233991488B90A39a2eb",
+        "contract": "0xd6E816b4EEe55e8C2eB7E3af5B60AaEf53AD256F",
         "interested": ["d1", "d2", "d3"],
         "funders": ["arne","linda","rigmor","wayne","garreth","carlos","joann","nieke"]
     }, {
@@ -40,7 +40,7 @@ module.exports = {
         "doctorsNeeded": 3,
         "description": "#big project \nThis is the long text to show, formated as markdown?",
         "status": "planned",
-        "contract": "0xbd37A601265d90EC90b9b233991488B90A39a2eb",
+        "contract": "0xd6E816b4EEe55e8C2eB7E3af5B60AaEf53AD256F",
         "interested": ["d1", "d2", "d3"],
         "funders": ["arne","linda","rigmor","wayne","garreth","carlos","joann","nieke"]
     }, {
@@ -55,7 +55,7 @@ module.exports = {
         "doctorsNeeded": 3,
         "description": "#big project \nThis is the long text to show, formated as markdown?",
         "status": "planned",
-        "contract": "0xbd37A601265d90EC90b9b233991488B90A39a2eb",
+        "contract": "0xd6E816b4EEe55e8C2eB7E3af5B60AaEf53AD256F",
         "interested": ["d1", "d2", "d3"],
         "funders": ["arne","linda","rigmor","wayne","garreth","carlos","joann","nieke"]
     }
